27adeae584de5c9ba05b113987e33dcff5bf7779
mahadi24t <mahadi24t@outlook.com>
2025-08-12 02:11:17 +0600

    intialheader

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import chefLogo from '../images/ai-chef.png';
+export default function Header() {
+    return (
+        <header>
+            <img src={chefLogo}/>
+            <h2>AI CHEF</h2>
+        </header>
+    )
+}
