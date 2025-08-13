e1e6255e4c560f1bd746be547121b97ea5e71e06
mahadi24t <mahadi24t@outlook.com>
2025-08-13 21:53:29 +0600

    main

--- components/maino.jsx
+++ components/maino.jsx
@@ -1,13 +1,31 @@
 export default function Maino() {
+    const Ingradients = [
+        "Oregano",
+        "Chicken",
+        "Tomato",
+    ]
+    const ingradientsList = Ingradients.map(ingredient => 
+        (<li key={ingredient}>{ingredient}</li>
+     ))
+     function handlesubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        ingradientsList.push(newIngredient);
+        alert("Ingredient added"); 
+    }
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onClick={handlesubmit} className="add-ingredient-form">
                 <input 
                 type="text"
                 placeholder="e.g oregano"
                 aria-label="Add Ingradient" />
                 <button>Add Ingradient</button>
             </form>
+            <ul>
+                {ingradientsList}
+            </ul>
         </main>
     );
 }
